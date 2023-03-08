4c2ef02c75ddc4c76e3794ffa87c9ffae29cc184
Benjitanoo <benou84530@outlook.fr>
2023-03-08 11:14:35 +0100

    edit

---
+++ script.js
@@ -0,0 +1,112 @@
+// Variables globales
+let compteur = 0 // Compteur qui permettra de savoir sur quelle slide nous sommes
+let timer, elements, slides, slideWidth
+
+window.onload = () => {
+    // On récupère le conteneur principal du diaporama
+    const diapo = document.querySelector(".diapo")
+
+    // On récupère le conteneur de tous les éléments
+    elements = document.querySelector(".elements")
+
+    // On récupère un tableau contenant la liste des diapos
+    slides = Array.from(elements.children)
+
+    // On calcule la largeur visible du diaporama
+    slideWidth = diapo.getBoundingClientRect().width
+
+    // Automatiser le diaporama
+    timer = setInterval(slideNext, 4500)
+
+    // Mise en oeuvre du "responsive"
+    window.addEventListener("resize", () => {
+        slideWidth = diapo.getBoundingClientRect().width
+        slideNext()
+    })
+}
+
+/**
+ * Cette fonction fait défiler le diaporama vers la droite
+ */
+function slideNext() {
+    // On incrémente le compteur
+    compteur++
+    // Si on dépasse la fin du diaporama, on "rembobine"
+    if (compteur == slides.length) {
+        compteur = 0
+    }
+
+    // On calcule la valeur du décalage
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
+/**
+ * Cette fonction fait défiler le diaporama vers la gauche
+ */
+function slidePrev() {
+    // On décrémente le compteur
+    compteur--
+
+    // Si on dépasse le début du diaporama, on repart à la fin
+    if (compteur < 0) {
+        compteur = slides.length - 1
+    }
+
+    // On calcule la valeur du décalage
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
+/**
+ * On stoppe le défilement
+ */
+function stopTimer() {
+    clearInterval(timer)
+}
+
+/**
+ * On redémarre le défilement
+ */
+function startTimer() {
+    timer = setInterval(slideNext, 3500)
+}
+
+// Récupération des données météo depuis l'API OpenWeatherMap
+const apiKey = '1e348b815215aac837e817503347beed'
+const city = 'Aix-en-Provence'
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=Aix-en-Provence&units=metric&appid=1e348b815215aac837e817503347beed&lang=fr`)
+    .then(response => response.json())
+    .then(data => {
+        // Récupération des informations météo
+        const temp = data.main.temp.toFixed(1) + '°C'
+        const desc = data.weather[0].description
+
+        // Affichage des informations météo dans le widget
+        document.querySelector('#weather-temp').textContent = temp
+        document.querySelector('#weather-desc').textContent = desc
+    })
+    .catch(error => console.log(error))
+
+    //Requete API SAINT ELOI
+    const url = 'https://steloi.ogia.fr/ogia_ateliers_api.php';
+
+    const data = {
+      "API_KEY": "5656FD9CF72B47AFBCCE4917CDDF196B",
+      "JOUR": "L"
+    };
+    
+    const headers = {
+      'Content-Type': 'application/json'
+    };
+    
+    fetch(url, {
+      method: 'POST',
+      headers: headers,
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+    
